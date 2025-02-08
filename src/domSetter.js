f6690988d551aa787f34c3ca49f02625f5d79166
mudakakk <161918354+mudakakk@users.noreply.github.com>
2025-02-08 17:21:53 +0000

    i improved the styling somwhat and also added a function which allows users to see each days individual hours under the 'daily' view

--- src/domSetter.js
+++ src/domSetter.js
@@ -1,3 +1,6 @@
+import chevronUp from "./chevron-up.svg";
+import chevronDown from "./chevron-down.svg";
+
 const displayWeather = function (processedDataObj, e) {
   const location = document.querySelector("#location");
   const mainInner = document.querySelector("#main-inner");
@@ -15,14 +18,21 @@ const displayWeather = function (processedDataObj, e) {
             </div>
         <div id="info-table">
             <h3 id="title">Current conditions</h3>
-            <p id="temp">${processedDataObj.curTemp}°</p>
+            <div id="temp-container">
+              <p id="temp">${processedDataObj.curTemp}°</p>
+              <p id="feels-like">Feels like ${
+                processedDataObj.curFeelsLike
+              }°</p>
+            </div>
             <p id="description">${processedDataObj.curConditions}</p>
-            <p id="precipitation-type">${
-              processedDataObj.curPrecipType
-                ? `${processedDataObj.curPrecipType} - ${processedDataObj.curPrecip}mm`
-                : "None"
-            }</p>
-            <p id="feels-like">Feels like ${processedDataObj.curFeelsLike}°</p>
+            <div id="precip-container">
+              <p id="precip-container-title">Precipitation:</p>
+              <p id="precipitation-type">${
+                processedDataObj.curPrecipType
+                  ? `${processedDataObj.curPrecipType} - ${processedDataObj.curPrecip}mm`
+                  : "None"
+              }</p>
+            </div>
             <p id="wind-speed">Windspeed: ${
               processedDataObj.curWindSpeed
             }m/s</p>
@@ -38,14 +48,21 @@ const displayWeather = function (processedDataObj, e) {
             </div>
         <div id="info-table">
             <h3 id="title">Current conditions</h3>
-            <p id="temp">${processedDataObj.curTemp}°</p>
+            <div id="temp-container">
+              <p id="temp">${processedDataObj.curTemp}°</p>
+              <p id="feels-like">Feels like ${
+                processedDataObj.curFeelsLike
+              }°</p>
+            </div>
             <p id="description">${processedDataObj.curConditions}</p>
-            <p id="precipitation-type">${
-              processedDataObj.curPrecipType
-                ? `${processedDataObj.curPrecipType} - ${processedDataObj.curPrecip}mm`
-                : "None"
-            }</p>
-            <p id="feels-like">Feels like ${processedDataObj.curFeelsLike}°</p>
+            <div id="precip-container">
+              <p id="precip-container-title">Precipitation:</p>
+              <p id="precipitation-type">${
+                processedDataObj.curPrecipType
+                  ? `${processedDataObj.curPrecipType} - ${processedDataObj.curPrecip}mm`
+                  : "None"
+              }</p>
+            </div>
             <p id="wind-speed">Windspeed: ${
               processedDataObj.curWindSpeed
             }m/s</p>
@@ -57,7 +74,10 @@ const displayWeather = function (processedDataObj, e) {
     for (const hour of processedDataObj.days[0].hours) {
       hours += `
         <tr class="hour">
-            <th scope="row" class="hour-time">${hour.dateTime}</th>
+            <th scope="row" class="hour-time">${hour.dateTime
+              .split("")
+              .slice(0, 5)
+              .join("")}</th>
             <td class="hour-temp">${hour.temp}</td>
             <td class="hour-conditions">${hour.conditions}</td>
             <td class="hour-precip-type">${
@@ -90,15 +110,20 @@ const displayWeather = function (processedDataObj, e) {
     let days = ``;
 
     for (const day of processedDataObj.days) {
+      let dayTemporary = day.dateTime.split("").slice(5);
+      dayTemporary[2] = ".";
+      dayTemporary = dayTemporary.join("");
+
       days += `
       <div class="day not-legend ${processedDataObj.days.indexOf(day)}">
-        <div class="day-date">${day.dateTime}</div>
+        <div class="day-date">${dayTemporary}</div>
         <div class="day-temp">${day.temp}</div>
         <div class="day-condition">${day.conditions}</div>
         <div class="day-feels-like">${day.feelsLike}</div>
         <div class="day-precip-type">${
           day.precipType ? `${day.precipType} - ${day.precip}mm` : "None"
         }</div>
+        <img class="expand-hide-symbol" src="${chevronDown}"/>
       </div>
       `;
     }
@@ -121,8 +146,70 @@ const displayWeather = function (processedDataObj, e) {
         ${days}
       </div>
     `;
-  }
 
+    // and here i just add the event listeners necessary to bring the following two functions together.
+
+    const addListenersToDays = function () {
+      document.querySelectorAll(".not-legend").forEach((dayEl) => {
+        dayEl.addEventListener(
+          "click",
+          (e) => {
+            if (!document.querySelector("#hourly-table")) createDayHours(dayEl);
+            dayEl.addEventListener("click", removeHourTableFromDay, {
+              once: true,
+            });
+          },
+          { once: true }
+        );
+      });
+    };
+
+    addListenersToDays();
+
+    // the following two functions are used to make each day in the daily view display the conditions for each hour of the day on demand
+    // the first creates the html for the hours of the day that was clicked on by the user
+
+    const createDayHours = function (dayEl) {
+      dayEl.children[5].src = chevronUp;
+      let hours = ``;
+      for (const hour of processedDataObj.days[dayEl.classList[2]].hours) {
+        hours += `
+            <tr class="hour">
+                <th scope="row" class="hour-time">${hour.dateTime
+                  .split("")
+                  .slice(0, 5)
+                  .join("")}</th>
+                <td class="hour-temp">${hour.temp}</td>
+                <td class="hour-conditions">${hour.conditions}</td>
+                <td class="hour-precip-type">${
+                  hour.precipType
+                    ? `${hour.precipType} - ${hour.precip}mm`
+                    : "None"
+                }</td>
+            </tr>
+            `;
+      }
+      dayEl.innerHTML += `
+        <table id="hourly-table">
+            <thead>
+                <th scope="col">Hour</th>
+                <th scope="col">Temp</th>
+                <th scope="col">Conditions</th>
+                <th scope="col">Precipitation</th>
+            </thead>
+            ${hours}
+        </table>
+      `;
+    };
+
+    // the second is used to hide the hours of the day once the user clicks on it again, also removing the event listener used to hide the hours
+
+    const removeHourTableFromDay = function (ev) {
+      ev.currentTarget.children[5].src = chevronDown;
+      ev.currentTarget.removeChild(ev.currentTarget.children[6]);
+      addListenersToDays();
+    };
+  }
   document.querySelectorAll(".period-btn").forEach((btn) => {
     btn.addEventListener("click", (ev) => {
       displayWeather(processedDataObj, ev);
